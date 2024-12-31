975e1b149f8388b89b2899d495d7f45459d00867
Vskurilov <vskurilov@gmail.com>
2024-12-31 17:36:08 +0300

    add script types

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,5 +1,7 @@
 import { TrpcProvider } from '../lib/trpc'
 import { AllideasPage } from '../pages/AllideasPage/index'
+
+const ssst:string = 1
 export const App = () => {
   return (
     <TrpcProvider>
