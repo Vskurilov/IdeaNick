62d840ce7a152192fc83062503c37a5d3a10f8de
Vskurilov <vskurilov@gmail.com>
2024-12-31 17:17:36 +0300

    install trpc on frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import * as trpcExpress from '@trpc/server/adapters/express'
 import { trpcRouter } from './trpc'
-
+import corm from 'cors'
 const expressApps = express()
 
 expressApps.get('/ping', (req, res) => {
     res.send('pong')
 })
+
+expressApps.use(corm())
 
 expressApps.use('/trpc', trpcExpress.createExpressMiddleware({
 router: trpcRouter,
---
+++ webapp/pages/AllideasPage/index.tsx
@@ -0,0 +1,26 @@
+import { trpc } from '../../lib/trpc'
+export const AllideasPage = () => {
+    
+const result = trpc.getIdeas.useQuery()
+    console.log(result)
+    const { data, error, isLoading, isFetching, isError} = trpc.getIdeas.useQuery()    
+    if(isLoading || isFetching) {
+        return <div>Loading...</div>
+    }
+    if(isError) {
+        return <div>Error: {error.message}</div>
+    }    
+    return (
+      <div>
+        <h1>IdeaNick</h1>
+        {data.ideas.map((idea) => {
+          return (
+            <div key={idea.nick}>
+              <h2>{idea.name}</h2>
+              <p>{idea.description}</p>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
