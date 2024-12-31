5ac1271fe093210c8e339acb41000b0672ff2227
Vskurilov <vskurilov@gmail.com>
2024-12-31 18:17:01 +0300

    monorepo scripts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,18 +3,19 @@ import * as trpcExpress from '@trpc/server/adapters/express'
 import { trpcRouter } from './trpc'
 import corm from 'cors'
 const expressApps = express()
-
 expressApps.get('/ping', (req, res) => {
-    res.send('pong')
+  res.send('pong')
 })
 
 expressApps.use(corm())
 
-expressApps.use('/trpc', trpcExpress.createExpressMiddleware({
-router: trpcRouter,
-})
+expressApps.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: trpcRouter,
+  })
 )
 
 expressApps.listen(3000, () => {
-    console.info('Listening at http://localhost:3000');
+  console.info('Listening at http://localhost:3000')
 })
--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,7 +1,6 @@
 import { TrpcProvider } from '../lib/trpc'
 import { AllideasPage } from '../pages/AllideasPage/index'
 
-const ssst:string = 1
 export const App = () => {
   return (
     <TrpcProvider>
